6b3b42576bcfc6393d3ad9adc3bd18954160ce9a
Cyborg2017 <wangyang0987654@gmail.com>
2026-03-26 21:34:38 +0800

    feat: add support for new T0xAC device(22013201)
    * Midea KFR-35G/N8MXC1ⅡPro

--- custom_components/midea_smart_home/device_mapping/T0xAC.py
+++ custom_components/midea_smart_home/device_mapping/T0xAC.py
@@ -366,6 +366,144 @@ DEVICE_MAPPING = {
             }
         }
     },
+    "22013201": {
+        "rationale": ["off", "on"],
+        "initial_query": [
+            {},
+            {"prevent_straight_wind"},
+            {"prevent_super_cool"},
+            {"wind_swing_lr_angle"},
+            {"wind_swing_ud_angle"}
+        ],
+        "centralized": ["buzzer"],
+        "entities": {
+            Platform.CLIMATE: {
+                "air_conditioner": {
+                    "power": "power",
+                    "hvac_modes": {
+                        "off": {"power": "off"},
+                        "heat": {"power": "on", "mode": "heat"},
+                        "cool": {"power": "on", "mode": "cool"},
+                        "auto": {"power": "on", "mode": "auto"},
+                        "dry": {"power": "on", "mode": "dry"},
+                        "fan_only": {"power": "on", "mode": "fan"}
+                    },
+                    "preset_modes": {
+                        "none": {"cool_power_saving": 0},
+                        "eco": {"cool_power_saving": 1}
+                    },
+                    "swing_modes": {
+                        "off": {"wind_swing_lr": "off", "wind_swing_ud": "off"},
+                        "both": {"wind_swing_lr": "on", "wind_swing_ud": "on"},
+                        "horizontal": {"wind_swing_lr": "on", "wind_swing_ud": "off"},
+                        "vertical": {"wind_swing_lr": "off", "wind_swing_ud": "on"},
+                    },
+                    "fan_modes": {
+                        "20": {"wind_speed": 20},
+                        "40": {"wind_speed": 40},
+                        "60": {"wind_speed": 60},
+                        "80": {"wind_speed": 80},
+                        "100": {"wind_speed": 100},
+                        "102": {"wind_speed": 102}
+                    },
+                    "target_temperature": ["temperature", "small_temperature"],
+                    "current_temperature": "indoor_temperature",
+                    "pre_mode": "mode",
+                    "aux_heat": "ptc",
+                    "min_temp": 17,
+                    "max_temp": 30,
+                    "temperature_unit": UnitOfTemperature.CELSIUS,
+                    "precision": PRECISION_HALVES,
+                }
+            },
+            Platform.SELECT: {
+                "wind_swing_ud_angle": {
+                    "options": {
+                        "off": {"wind_swing_ud_angle": 0},
+                        "top": {"wind_swing_ud_angle": 1},
+                        "upper": {"wind_swing_ud_angle": 25},
+                        "middle": {"wind_swing_ud_angle": 50},
+                        "lower": {"wind_swing_ud_angle": 75},
+                        "bottom": {"wind_swing_ud_angle": 100}
+                    }
+                },
+                "wind_swing_lr_angle": {
+                    "options": {
+                        "off": {"wind_swing_lr_angle": 0},
+                        "leftmost": {"wind_swing_lr_angle": 1},
+                        "left": {"wind_swing_lr_angle": 25},
+                        "middle": {"wind_swing_lr_angle": 50},
+                        "right": {"wind_swing_lr_angle": 75},
+                        "rightmost": {"wind_swing_lr_angle": 100}
+                    }
+                },
+                "wind_around": {
+                    "options": {
+                        "off": {"wind_around": "off"},
+                        "up": {"wind_around": "on", "wind_around_ud": 1},
+                        "down": {"wind_around": "on", "wind_around_ud": 2}
+                    }
+                },
+                "prevent_straight_wind": {
+                    "options": {
+                        "off": {"prevent_straight_wind": 1},
+                        "up": {"prevent_straight_wind": 2, "prevent_straight_wind_lr": 2},
+                        "down": {"prevent_straight_wind": 2, "prevent_straight_wind_lr": 3}
+                    }
+                },
+                "ptc": {
+                    "options": {
+                        "off": {"ptc": "off"},
+                        "mode_1": {"ptc": "on", "ptc_default_rule": 1},
+                        "mode_2": {"ptc": "on", "ptc_default_rule": 0}
+                    }
+                }
+            },
+            Platform.SWITCH: {
+                "power": {
+                    "device_class": SwitchDeviceClass.SWITCH
+                },
+                "buzzer": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "default_value": "on"
+                },
+                "screen_display": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": [0, 100],
+                    "translation_key": "display_on_off"
+                },
+                "prevent_super_cool": {
+                    "device_class": SwitchDeviceClass.SWITCH
+                },
+                "dry": {
+                    "device_class": SwitchDeviceClass.SWITCH
+                },
+                "light_sensitive": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": [0, 3]
+                },
+                "power_saving": {
+                    "device_class": SwitchDeviceClass.SWITCH
+                }
+            },
+            Platform.SENSOR: {
+                "mode": {
+                    "device_class": SensorDeviceClass.ENUM,
+                },
+                "indoor_temperature": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT,
+                    "translation_key": "cur_temperature"
+                },
+                "outdoor_temperature": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                }
+            }
+        }
+    },
     ("22040055", "22040023", "22040053"): {
         "rationale": ["off", "on"],
         "initial_query": [
