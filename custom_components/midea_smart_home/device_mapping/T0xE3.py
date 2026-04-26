cbc9d83b9df71e34a326258f24a74002339c59cf
Cyborg2017 <wangyang0987654@gmail.com>
2026-04-26 18:32:05 +0800

    feat(T0xE3): add support for device 5110186B water heater

--- custom_components/midea_smart_home/device_mapping/T0xE3.py
+++ custom_components/midea_smart_home/device_mapping/T0xE3.py
@@ -132,5 +132,81 @@ DEVICE_MAPPING = {
                 }
             }
         }
+    },
+    "5110186B": {
+        "rationale": ["off", "on"],
+        "entities": {
+            Platform.WATER_HEATER: {
+                "water_heater": {
+                    "power": "power",
+                    "operation_list": {
+                        "off": {"power": "off"},
+                        "heat": {"power": "on", "mode": "shower"}
+                    },
+                    "target_temperature": "temperature",
+                    "current_temperature": "out_water_tem",
+                    "min_temp": 32,
+                    "max_temp": 65,
+                    "temperature_unit": UnitOfTemperature.CELSIUS,
+                    "precision": PRECISION_WHOLE
+                }
+            },
+            Platform.SWITCH: {
+                "power": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "cold_water_master": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "cold_water_dot": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "cold_water_pressure": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "change_litre_switch": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                }
+            },
+            Platform.SELECT: {
+                "cold_water_conservation": {
+                    "options": {
+                        "off": {"cold_water_conservation": "off"},
+                        "on": {"cold_water_conservation": "on"}
+                    }
+                },
+                "mode": {
+                    "translation_key": "e3_mode",
+                    "options": {
+                        "shower": {"mode": "shower"},
+                        "kitchen": {"mode": "kitchen"},
+                        "high_temperature": {"mode": "high_temperature"}
+                    }
+                }
+            },
+            Platform.SENSOR: {
+                "out_water_tem": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "return_water_tem": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "in_water_tem": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "temperature": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT,
+                    "translation_key": "temp_set"
+                }
+            }
+        }
     }
 }
