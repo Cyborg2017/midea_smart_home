9364d3901358028d1fe22325f0c27b2b0382499c
Cyborg2017 <wangyang0987654@gmail.com>
2026-08-16 21:31:30 +0800

    feat: add support for T0xAD air sensor devices
    Change-Id: I38ae0053cbb18999b02443f0564c1ac9f24f3e27

--- custom_components/midea_smart_home/const.py
+++ custom_components/midea_smart_home/const.py
@@ -52,6 +52,7 @@ DEVICE_TYPES = {
     0x9C: "Integrated Stove",
     0xA1: "Dehumidifier",
     0xAC: "Floor Air Conditioner / Wall Air Conditioner / Central Air Conditioner / Central Fresh Air / Central Miniaturized Fresh Air",
+    0xAD: "Air Sensor",
     0xB0: "Microwave Oven",
     0xB6: "Range Hood",
     0xB8: "Smart Robot Vacuum",
